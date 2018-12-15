8a3c9ccc9686c385806f43fd43ba24174a528909
Thomas Siew <thomas.s@Australasia.SelectNET.com>
2018-12-15 11:33:53 +0800

    2.8

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose')
+
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 const app = express();
 
 //DB config
@@ -13,6 +17,12 @@ mongoose
 
 
 app.get('/', (req,res) => res.send('Hello!'));
+
+// Use Routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
 
 const port = process.env.PORT || 5000;
 
