91f283f98dff0a5e9da6c3edfb2f69a495c3ce59
Thomas Siew <thomas.s@Australasia.SelectNET.com>
2018-12-16 15:42:03 +0800

    next 13

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:"mongodb://thomassiew007:asd123@ds235302.mlab.com:35302/merndb"
+    mongoURI:"mongodb://thomassiew007:asd123@ds235302.mlab.com:35302/merndb",
+    secretOrKey: 'secret'
 }
